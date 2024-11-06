91cd9b516df376fc939622223266995f83600463
RAArrieta <alfredoarrieta99@gmail.com>
2024-11-06 16:36:20 -0300

    .

--- donQuijoteWeb/config/settings.py
+++ donQuijoteWeb/config/settings.py
@@ -11,12 +11,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = get_random_secret_key()
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['SanchoPizza.pythonanywhere.com']
 
 
 # Application definition
